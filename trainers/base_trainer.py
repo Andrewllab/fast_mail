035a4436d8eacb422952817c7c2b7eafb27d1855
xiaogangjia <jia266163@gmail.com>
2025-01-02 15:23:17 +0100

    import change

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -16,9 +16,9 @@ from torch.utils.data.distributed import DistributedSampler
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel
 
-from fast_mail.agents.utils.scaler import Scaler, ActionScaler, MinMaxScaler
-from fast_mail.agents.utils.ema import ExponentialMovingAverage
-from fast_mail.agents.base_agent import BaseAgent
+from agents.utils.scaler import Scaler, ActionScaler, MinMaxScaler
+from agents.utils.ema import ExponentialMovingAverage
+from agents.base_agent import BaseAgent
 
 log = logging.getLogger(__name__)
 
@@ -134,7 +134,7 @@ class BaseTrainer:
         agent.store_model_weights(agent.working_dir, sv_name='last_model.pth')
         # or send weight out of the class
 
-    def train_one_step(self, agent: BaseAgent , obs_dict, action):
+    def train_one_step(self, agent: BaseAgent, obs_dict, action):
         """Run a single training step."""
         agent.train()
 
