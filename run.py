13affa3baa039a560e7b1739bc647e43d6f2c6ed
xiaogangjia <jia266163@gmail.com>
2025-01-03 17:05:01 +0100

    add xlstm params

--- run.py
+++ run.py
@@ -59,7 +59,9 @@ def main(cfg: DictConfig) -> None:
         else:
             if(agent.vqvae.load_model(cfg.pretrain_vqvae_path)):
                 log.info(f"Loaded pretrained vqvae from {cfg.pretrain_vqvae_path}")
-    
+
+    agent.get_params()
+
     trainer.main(agent)
 
     # simulate the model
