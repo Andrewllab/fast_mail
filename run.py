b6ef38ae06045a456b20393516e0accd399beb5a
xiaogangjia <jia266163@gmail.com>
2025-01-03 16:23:38 +0100

    enable simulation

--- run.py
+++ run.py
@@ -62,17 +62,17 @@ def main(cfg: DictConfig) -> None:
     
     trainer.main(agent)
 
-    # # simulate the model
-    # env_sim = hydra.utils.instantiate(cfg.simulation)
-    # env_sim.get_task_embs(trainer.trainset.tasks)
-    # #
-    # # sv_dir = sim_framework_path("pretrain_weights")
-    # #
-    # env_sim.test_agent(agent, cfg.agents, epoch=cfg.epoch)
+    # simulate the model
+    env_sim = hydra.utils.instantiate(cfg.simulation)
+    env_sim.get_task_embs(trainer.trainset.tasks)
     #
-    # log.info("Training done")
-    # log.info("state_dict saved in {}".format(agent.working_dir))
-    # wandb.finish()
+    # sv_dir = sim_framework_path("pretrain_weights")
+    #
+    env_sim.test_agent(agent, cfg.agents, epoch=cfg.epoch)
+
+    log.info("Training done")
+    log.info("state_dict saved in {}".format(agent.working_dir))
+    wandb.finish()
 
 
 if __name__ == "__main__":
