a2a7f39838acac82c6ab173190238348ffa94339
xiaogangjia <jia266163@gmail.com>
2025-01-05 18:15:12 +0100

    scripts

--- script/bc/dec/transformer.sh
+++ script/bc/dec/transformer.sh
@@ -1,8 +1,7 @@
 python run.py  --config-name=libero_horeka_config \
             --multirun agents=bc_agent \
             agent_name=bc_transformer \
-            group=bc_decoder_only_non_ema \
+            group=bc_decoder_only \
             agents/model=bc/bc_dec_transformer \
             task_suite=libero_spatial \
-            if_use_ema=False \
             seed=0,1,2
--- script/bc/dec/xlstm.sh
+++ script/bc/dec/xlstm.sh
@@ -1,8 +1,7 @@
 python run.py  --config-name=libero_horeka_config \
             --multirun agents=bc_agent \
             agent_name=bc_xlstm \
-            group=bc_decoder_only_non_ema \
+            group=bc_decoder_only \
             agents/model=bc/bc_dec_xlstm \
-            task_suite=libero_spatial \
-            if_use_ema=False \
+            task_suite=libero_object,libero_goal,libero_10,libero_spatial \
             seed=0,1,2
