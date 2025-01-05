93a9f228eb2923653f04fc91e4845cdea003be64
xiaogangjia <jia266163@gmail.com>
2025-01-05 18:43:36 +0100

    scripts

--- script/bc/encdec/transformer.sh
+++ script/bc/encdec/transformer.sh
@@ -3,5 +3,5 @@ python run.py  --config-name=libero_horeka_config \
             agent_name=bc_transformer \
             group=bc_encoder_decoder \
             agents/model=bc/bc_encdec_transformer \
-            task_suite=libero_spatial \
-            seed=0
+            task_suite=libero_object,libero_goal,libero_10,libero_spatial \
+            seed=0,1,2
