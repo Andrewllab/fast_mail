836af50f8a648e21f1ff12a97a7209b025ccf952
xiaogangjia <jia266163@gmail.com>
2025-01-07 21:58:35 +0100

    film resnet

--- run_test.py
+++ run_test.py
@@ -28,7 +28,7 @@ def set_seed_everywhere(seed):
     random.seed(seed)
 
 
-@hydra.main(config_path="configs", config_name="robocasa_config.yaml", version_base="1.3")
+@hydra.main(config_path="configs", config_name="robocasa_horeka_config.yaml", version_base="1.3")
 def main(cfg: DictConfig) -> None:
 
     set_seed_everywhere(cfg.seed)
@@ -51,20 +51,20 @@ def main(cfg: DictConfig) -> None:
     agent.get_params()
     trainer.main(agent)
 
-    agent.set_scaler(trainer.scaler)
+    # agent.set_scaler(trainer.scaler)
     # agent.load_pretrained_model('/home/david/2025/fast_mail/logs/PnPCabToCounter/sweeps/beso/2025-01-07/11-01-58', sv_name='last_model')
 
-    # simulate the model
-    env_sim = hydra.utils.instantiate(cfg.simulation)
-    # env_sim.get_task_embs(trainer.trainset.tasks)
+    # # simulate the model
+    # env_sim = hydra.utils.instantiate(cfg.simulation)
+    # # env_sim.get_task_embs(trainer.trainset.tasks)
+    # #
+    # # sv_dir = sim_framework_path("pretrain_weights")
+    # #
+    # env_sim.test_agent(agent, cfg.agents, epoch=cfg.epoch)
     #
-    # sv_dir = sim_framework_path("pretrain_weights")
-    #
-    env_sim.test_agent(agent, cfg.agents, epoch=cfg.epoch)
-
-    log.info("Training done")
-    log.info("state_dict saved in {}".format(agent.working_dir))
-    wandb.finish()
+    # log.info("Training done")
+    # log.info("state_dict saved in {}".format(agent.working_dir))
+    # wandb.finish()
 
 
 if __name__ == "__main__":
