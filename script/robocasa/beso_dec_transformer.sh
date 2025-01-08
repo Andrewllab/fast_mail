4f16ade3f4cfd53d6278e503ce7a9551e131c32e
xiaogangjia <jia266163@gmail.com>
2025-01-08 13:45:07 +0100

    script

---
+++ script/robocasa/beso_dec_transformer.sh
@@ -0,0 +1,9 @@
+python run.py  --config-name=robocasa_horeka_config \
+            --multirun agents=beso_agent \
+            agent_name=beso_transformer \
+            group=beso_decoder_only \
+            agents/model=beso/beso_dec_transformer \
+            epoch=100,200 \
+            n_embd=256,512 \
+            encoder_n_layer=6 \
+            seed=0
