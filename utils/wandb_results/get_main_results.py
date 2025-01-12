2984be0e081f1ee92e9e264c9ddb21f4540598dd
xiaogangjia <jia266163@gmail.com>
2025-01-12 20:43:10 +0100

    script

--- utils/wandb_results/get_main_results.py
+++ utils/wandb_results/get_main_results.py
@@ -20,7 +20,7 @@ class bcolors:
 config = {
     "local": {
         'entity': 'tiger_or_cat',
-        'project': 'fast_mail_benchmark',
+        'project': 'icml_libero',
         'groups': '',
         'fields': '',
         'runs': ["all"],
@@ -35,10 +35,8 @@ if __name__ == '__main__':
 
     task_suite = ["libero_spatial", "libero_object", "libero_goal", "libero_10"]
 
-    store = ['max', 'mean']
-
-    groups = ['bc_decoder_only', 'bc_encoder_decoder', 'beso_decoder_only', 'beso_encoder_decoder']
-    fields = ['epoch100_average_success', 'None']
+    groups = ['bc_decoder_only', 'bc_encoder_decoder', 'beso_decoder_only', 'beso_encoder_decoder', 'fm_decoder_only']
+    fields = ['epoch100_average_success']
 
     # csv_file = open(sv_dir + '/bc_dec_results.csv', 'w', newline='')
     # writer = csv.writer(csv_file)
@@ -59,7 +57,8 @@ if __name__ == '__main__':
         for agent_name in agent_names:
 
             strings = []
-            task_success_rates = []
+            task_success_rates = {10: [],
+                                  50: []}
 
             for task in task_suite:
 
@@ -70,35 +69,42 @@ if __name__ == '__main__':
                 # success_list = []
                 for evaluation in fields:
 
-                    config['local']['groups'] = [group]
-                    config['local']['fields'] = [evaluation]
+                    for num_traj in [10, 50]:
 
-                    config['local']['config'] = {
-                        'task_suite': {
-                            'values': [task]
-                        },
-                        'agent_name': {
-                            'values': [agent_name]
+                        config['local']['groups'] = [group]
+                        config['local']['fields'] = [evaluation]
+
+                        config['local']['config'] = {
+                            'task_suite': {
+                                'values': [task]
+                            },
+                            'agent_name': {
+                                'values': [agent_name]
+                            },
+                            'traj_per_task': {
+                                'values': [num_traj]
+                            },
                         }
-                    }
 
-                    data_dict, config_list = wandb2numpy.export_data(config)
+                        data_dict, config_list = wandb2numpy.export_data(config)
 
-                    if len(data_dict) == 0 or len(data_dict['local'][evaluation]) == 0:
-                        strings.append("& -")
-                        continue
+                        if len(data_dict) == 0 or len(data_dict['local'][evaluation]) == 0:
+                            strings.append("& -")
+                            continue
 
-                    success = data_dict['local'][evaluation]
-                    success = np.squeeze(success)
+                        success = data_dict['local'][evaluation]
+                        success = np.squeeze(success)
 
-                    mean_result = success.mean() * 100
-                    std_result = success.std() * 100
+                        mean_result = success.mean() * 100
+                        std_result = success.std() * 100
 
-                    number_string = f"& ${mean_result:.1f} \scriptstyle \pm {std_result:.1f}$"
-                    print(bcolors.WARNING + number_string + bcolors.ENDC)
+                        number_string = f"& ${mean_result:.1f} \scriptstyle \pm {std_result:.1f}$"
+                        print(bcolors.WARNING + number_string + bcolors.ENDC)
 
-                    strings.append(number_string)
-                    task_success_rates.append(mean_result)
+                        strings.append(number_string)
+
+                        task_success_rates[num_traj].append(mean_result)
+                        # task_success_rates.append(mean_result)
                     # success_list.append(success)
 
                 # success_list = np.concatenate(success_list, axis=-1)
@@ -110,11 +116,13 @@ if __name__ == '__main__':
                 # writer.writerow([''])
                 # writer.writerow([''])
 
-            mean_task_success_rates = np.array(task_success_rates).mean()
-            mean_string = f"& ${mean_task_success_rates:.1f}$"
+            traj10_task_success_rates = np.array(task_success_rates[10]).mean()
+            traj50_task_success_rates = np.array(task_success_rates[50]).mean()
+            # mean_string = f"& ${mean_task_success_rates:.1f}$"
 
             print(bcolors.WARNING + f'-------------------{group},{agent_name}-------------------\n' + bcolors.ENDC)
             print(agent_names, task_suite)
             for string in strings:
                 print(string)
-            print(mean_string)
+            print(f"& ${traj10_task_success_rates:.1f}$")
+            print(f"& ${traj50_task_success_rates:.1f}$")
