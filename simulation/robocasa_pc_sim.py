c5e6be6d27837467506b472fc0fda88b3abe3186
xiaogangjia <jia266163@gmail.com>
2025-01-13 20:47:58 +0100

    add robocasa pc

--- simulation/robocasa_pc_sim.py
+++ simulation/robocasa_pc_sim.py
@@ -1,7 +1,12 @@
+import sys
+sys.path.append("/home/david/2025")
+
 import logging
 
+import cv2
 import einops
 import numpy as np
+import wandb
 import robosuite.utils.transform_utils as T
 import torch
 from omegaconf import ListConfig
@@ -43,8 +48,10 @@ class RoboCasaSim(BaseSim):
         if_vision: bool = False,
         pc_num_points: int = 1024,
         obj_max_num_points: int = 512,
+        use_full_point_cloud: bool = False,
         use_segmented_point_cloud: bool = False,
         global_action: bool = False,
+        use_pc_color: bool = False,
     ):
         super().__init__(seed, device, render, n_cores, if_vision)
 
@@ -53,107 +60,136 @@ class RoboCasaSim(BaseSim):
         self.camera_names = list(camera_names)
         self.global_action = global_action
 
-        self._init_env(
-            env_name,
-            img_width,
-            img_height,
-            render,
-            pc_num_points,
-            obj_max_num_points,
-            use_segmented_point_cloud,
-        )
+        self.env_name = env_name
 
-        self.pc_key = (
-            "segmented_sampled_point_cloud"
-            if use_segmented_point_cloud
-            else "sampled_point_cloud"
-        )
+        self.img_height = img_height
+        self.img_width = img_width
+
+        self.use_pc_color = use_pc_color
+        self.pc_num_points = pc_num_points
+        self.obj_max_num_points = obj_max_num_points
+        self.use_segmented_point_cloud = use_segmented_point_cloud
+
+        if use_full_point_cloud:
+            if use_segmented_point_cloud:
+                self.pc_key = "segmented_point_cloud"
+            else:
+                self.pc_key = "point_cloud"
+        else:
+            if use_segmented_point_cloud:
+                self.pc_key = "segmented_sampled_point_cloud"
+            else:
+                self.pc_key = "sampled_point_cloud"
 
         self.pos_key = "robot0_eef_pos" if global_action else "robot0_base_to_eef_pos"
         self.quat_key = (
             "robot0_eef_quat" if global_action else "robot0_base_to_eef_quat"
         )
 
+        # if render:
+        #     self.env.reset()
+        #     self.env.render()
+        #     cv2.destroyAllWindows()
+
         cprint(f"Using point cloud key: {self.pc_key}", "blue")
         cprint(f"Using position key: {self.pos_key}", "blue")
         cprint(f"Using quaternion key: {self.quat_key}", "blue")
 
-    def test_agent(self, agent: BaseAgent, cpu_set, epoch):
-        success_count = 0
-        task_completion_hold_count = -1
+    def test_agent(self, agent: BaseAgent):
 
-        for i in range(self.num_episode):
-            obs = self.env.reset()
+        for env in self.env_name:
 
-            if self.render:
-                self.env.render()
+            print(f"Initializing environment {env}")
 
-            agent.reset()
+            self._init_env(
+                env,
+                self.img_width,
+                self.img_height,
+                self.render,
+                self.pc_num_points,
+                self.obj_max_num_points,
+                self.use_segmented_point_cloud
+            )
 
-            lang = self.env.get_ep_meta()["lang"]
+            success_count = 0
+            task_completion_hold_count = -1
 
-            for j in tqdm(range(self.max_step_per_episode)):
-                obs_dict = {}
-                obs_dict["lang"] = lang
-
-                gripper_state = torch.from_numpy(obs["robot0_gripper_qpos"]).float()
-                obs_dict["gripper_state"] = einops.rearrange(
-                    gripper_state, "d -> 1 1 d"
-                ).to(self.device)
-
-                eef_pos = torch.from_numpy(obs[self.pos_key]).float()
-                obs_dict["eef_pos"] = einops.rearrange(eef_pos, "d -> 1 1 d").to(
-                    self.device
-                )
-
-                eef_quat = torch.from_numpy(obs[self.quat_key]).float()
-                obs_dict["eef_quat"] = einops.rearrange(eef_quat, "d -> 1 1 d").to(
-                    self.device
-                )
-
-                sampled_point_cloud = torch.from_numpy(obs[self.pc_key][:, :3]).float()
-                obs_dict["point_cloud"] = einops.rearrange(
-                    sampled_point_cloud, "num_points d -> 1 1 num_points d"
-                ).to(self.device)
-
-                action = agent.predict(obs_dict).cpu().numpy()
-
-                action = np.concatenate(
-                    [action[1:], action[:1], np.array([0, 0, 0, 0, -1])]
-                )
-                if self.global_action:
-                    action = self.get_local_action(action)
-
-                obs, _, done, _ = self.env.step(action)
+            for i in range(self.num_episode):
+                obs = self.env.reset()
 
                 if self.render:
                     self.env.render()
 
-                if self.env._check_success():
-                    if task_completion_hold_count > 0:
-                        task_completion_hold_count -= (
-                            1  # latched state, decrement count
-                        )
+                agent.reset()
+
+                lang = self.env.get_ep_meta()["lang"]
+
+                for j in tqdm(range(self.max_step_per_episode)):
+                    obs_dict = {}
+                    obs_dict["lang"] = lang
+
+                    gripper_state = torch.from_numpy(obs["robot0_gripper_qpos"][:1]).float()
+                    obs_dict["gripper_state"] = einops.rearrange(
+                        gripper_state, "d -> 1 1 d"
+                    ).to(self.device)
+
+                    eef_pos = torch.from_numpy(obs[self.pos_key]).float()
+                    obs_dict["eef_pos"] = einops.rearrange(eef_pos, "d -> 1 1 d").to(
+                        self.device
+                    )
+
+                    eef_quat = torch.from_numpy(obs[self.quat_key]).float()
+                    obs_dict["eef_quat"] = einops.rearrange(eef_quat, "d -> 1 1 d").to(
+                        self.device
+                    )
+
+                    sampled_point_cloud = torch.from_numpy(obs[self.pc_key]).float()
+                    if not self.use_pc_color:
+                        sampled_point_cloud = sampled_point_cloud[:, :3]
+                    obs_dict["point_cloud"] = einops.rearrange(
+                        sampled_point_cloud, "num_points d -> 1 1 num_points d"
+                    ).to(self.device)
+
+                    action = agent.predict(obs_dict).cpu().numpy()
+
+                    action = np.concatenate(
+                        [action, np.array([0, 0, 0, 0, -1])]
+                    )
+                    if self.global_action:
+                        action = self.get_local_action(action)
+
+                    obs, _, done, _ = self.env.step(action)
+
+                    if self.render:
+                        self.env.render()
+
+                    if self.env._check_success():
+                        if task_completion_hold_count > 0:
+                            task_completion_hold_count -= (
+                                1  # latched state, decrement count
+                            )
+                        else:
+                            task_completion_hold_count = (
+                                10  # reset count on first success timestep
+                            )
                     else:
                         task_completion_hold_count = (
-                            10  # reset count on first success timestep
-                        )
-                else:
-                    task_completion_hold_count = (
-                        -1
-                    )  # null the counter if there's no success
+                            -1
+                        )  # null the counter if there's no success
 
-                if task_completion_hold_count == 0:
-                    success_count += 1
-                    done = True
+                    if task_completion_hold_count == 0:
+                        success_count += 1
+                        done = True
 
-                if done:
-                    break
+                    if done:
+                        break
 
-        success_rate = success_count / self.num_episode
-        print(f"Success rate: {success_rate}")
+            success_rate = success_count / self.num_episode
+            print(f"{env} Success rate: {success_rate}")
 
-        return success_rate
+            wandb.log({f"{env}_average_success": success_rate})
+
+            self.env.close()
 
     def _get_local_action(self, global_action: np.ndarray) -> np.ndarray:
         base_mat = self.env.sim.data.get_site_xmat(
