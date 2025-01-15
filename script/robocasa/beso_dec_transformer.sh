b78df99c7ada98b8a9f67882826887a3701cf4e0
xiaogangjia <jia266163@gmail.com>
2025-01-15 23:04:30 +0100

    script

--- script/robocasa/beso_dec_transformer.sh
+++ script/robocasa/beso_dec_transformer.sh
@@ -3,5 +3,7 @@ python run_test.py  --config-name=robocasa_horeka_config \
             agent_name=beso_transformer \
             group=beso_decoder_only_benchmark \
             agents/model=beso/beso_dec_transformer \
+            agents/obs_encoders=pretrained_resnet_film \
+            agents.if_film_condition=True \
             encoder_n_layer=6 \
             seed=0,1,2
