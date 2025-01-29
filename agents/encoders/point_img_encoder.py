22cec042d0afad5a9cfd41abbcb124b612732b02
xiaogangjia <jia266163@gmail.com>
2025-01-29 11:10:50 +0100

    scripts

--- agents/encoders/point_img_encoder.py
+++ agents/encoders/point_img_encoder.py
@@ -15,9 +15,16 @@ class PointMLPResNetEncoder(nn.Module):
         self.point_mlp_encoder = hydra.utils.instantiate(point_mlp_encoder)
         self.resnet_encoder = hydra.utils.instantiate(resnet_encoder)
 
+        self.align_mlp = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.LayerNorm(256),
+        )
+
     def forward(self, obs_dict, lang_cond=None):
 
         pc_emb = self.point_mlp_encoder(obs_dict["point_cloud"])
         resnet_emb = self.resnet_encoder(obs_dict, lang_cond)
+
+        resnet_emb = self.align_mlp(resnet_emb)
 
         return torch.cat([resnet_emb, pc_emb], dim=1)
