c5dab20fa3aaa91e82ba4f604d6afe52922c6d0d
Balazs Gyenes <balazsgyenes@users.noreply.github.com>
2025-03-06 08:19:40 +0100

    Add quality of life features from previous projects.
    Add logging config that always logs debug statements to file.
    Add custom resolvers for hydra config.
    Customize hydra output folders and override dirnames.
    Centralize path definitions.
    Add platform definitions.
    Add util to help with seeding.

---
+++ utils/seeding.py
@@ -0,0 +1,24 @@
+import logging
+import random
+
+import numpy as np
+import torch
+
+log = logging.getLogger(__name__)
+
+
+def get_rng(cfg):
+    seed = cfg.get("seed")
+    rng = np.random.default_rng(seed=seed)
+    if seed is None:
+        log.info(f"Using random seed {rng.bit_generator.seed_seq.entropy} for this run")
+
+    return rng
+
+
+def manual_seed(rng):
+    torch.manual_seed(rng.integers(np.iinfo(np.int64).max))
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(rng.integers(np.iinfo(np.int64).max))
+    np.random.seed(rng.integers(np.iinfo(np.int64).max))
+    random.seed(rng.integers(np.iinfo(np.int64).max))
