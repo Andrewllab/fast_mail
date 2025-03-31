34252bf7ffee0d330631a422f282a02ebe56c350
Balazs Gyenes <balazsgyenes@users.noreply.github.com>
2025-03-31 17:51:55 +0200

    Fix debug logging.
    Add support for root level already set to debug.
    Update debug configs.

--- utils/logging.py
+++ utils/logging.py
@@ -35,7 +35,7 @@ def configure_logging(logging_cfg: DictConfig) -> None:
     ----------
     always_log_debug_to_file : null | True | str | list[str]
         Debug messages from these modules (or all modules if True) should
-        always be logged to file, even if suppressed in console output.
+        always be logged to file, logging level is INFO.
     always_suppress_debug_from : null | str | list[str]
         Debug messages from these modules should never be logged to file,
         even if hydra.verbose=True. This setting overrides
@@ -46,41 +46,46 @@ def configure_logging(logging_cfg: DictConfig) -> None:
     )
     blacklist: str | list[str] = logging_cfg.get("always_suppress_debug_from", []) or []
     hydra_verbose: bool | str | list[str] = HydraConfig.get().verbose
+    root_logger = logging.getLogger()
+    root_debug = root_logger.level == logging.DEBUG
 
-    if hydra_verbose is not True:
-        # if hydra_verbose is True, all debug messages should be logged to all
-        # outputs anyway, so nothing needs to be changed
-        # if hydra_verbose is not True, we set root logger level to DEBUG and
-        # remove messages that are not from whitelisted modules with a filter
-        root_logger = logging.getLogger()
+    # mimic behavior of hydra.verbose
+    # if hydra.verbose is True or the root logger is already set to debug level,
+    # then all debug messages should be logged to all outputs anyway, so nothing
+    # needs to be changed
+    if not (hydra_verbose is True or root_debug):
+        # if hydra_verbose is False, or a (list of) module names, we set root
+        # logger level to DEBUG and remove messages that are not from
+        # whitelisted modules with a filter
         root_logger.setLevel("DEBUG")
 
         if hydra_verbose is False:
+            # filter out all debug messages, since no modules set to verbose
             verbose_set = set()
-            # only consider message level, since no loggers set to verbose
             console_filter = filter_debug
         else:
+            # filter out debug messages, except those from modules set to verbose
             verbose_set = (
                 {hydra_verbose}
                 if isinstance(hydra_verbose, str)
                 else set(hydra_verbose)
             )
-            # filter out if debug level, except loggers set to verbose
             console_filter = partial(filter_debug_w_whitelist, whitelist=verbose_set)
 
         console_handler = next(h for h in root_logger.handlers if h.name == "console")
         console_handler.addFilter(console_filter)
 
+        # apply whitelist for printing debug messages to log file
+        # if whitelist is True (print all debug messages to file), then just
+        # don't filter messages in the file handler
         if whitelist is not True:
-            # if whitelist is True (print all debug messages to file), then
-            # just don't filter messages in the file handler
-
+            # otherwise filter out debug messages, except those from modules
+            # set to verbose or whitelisted
             whitelist_set = (
                 {whitelist} if isinstance(whitelist, str) else set(whitelist)
             )
             verbose_or_whitelist = whitelist_set | verbose_set
 
-            # filter out if debug level, except loggers set to verbose or whitelisted
             file_filter = partial(
                 filter_debug_w_whitelist, whitelist=verbose_or_whitelist
             )
