037b8f5c1973be7f8a717c388546027601b7cddf
jan <jfrieling96@icloud.com>
2025-04-23 23:52:20 +0200

    Add transforms for point jitter + color jitter

---
+++ transforms/image_jitter_color.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+from typing import Union
+
+import torch
+from tensordict import TensorDict
+from torchvision.transforms.v2 import ColorJitter
+
+from environments.specs import CameraSpec, DataSpecs, RGBStream
+from transforms.base_transform import Transform
+
+
+class ColorJitterImage(Transform):
+    def __init__(
+        self,
+        specs: DataSpecs,
+        brightness: Union[float, tuple[float, float]],
+        contrast: Union[float, tuple[float, float]],
+        saturation: Union[float, tuple[float, float]],
+        hue: Union[float, tuple[float, float]],
+    ) -> None:
+
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+        self.hue = hue
+
+        # find the specs that this transform acts on
+        input_specs = {
+            key: spec
+            for key, spec in specs.obs.items()
+            if isinstance(spec, CameraSpec)
+            and any(isinstance(stream, RGBStream) for stream in spec.streams.values())
+        }
+        self._input_specs = input_specs
+
+        # create a modified specs object for the output
+        obs_specs = dict(specs.obs)  # copy obs specs for local modification
+        for key, spec in input_specs.items():
+            streams = dict(spec.streams)  # copy streams for local modification
+            for name, stream in streams.items():
+                stream = stream.reorder_channels("CHW")
+                streams[name] = stream
+            obs_specs[key] = spec.replace(streams=streams)
+        self._output_specs = specs.replace(obs=obs_specs)
+
+    @property
+    def specs(self) -> DataSpecs:
+        return self._output_specs
+
+    def __call__(self, tensordict: TensorDict) -> TensorDict:
+        default_float_dtype = torch.get_default_dtype()
+
+        for key, spec in self._input_specs.items():
+            images = tensordict["obs", key]
+            for name, stream in spec.streams.items():
+                if not isinstance(stream, RGBStream):
+                    continue
+
+                image = images[name]
+
+                if stream.channel_order == "HWC":
+                    image = torch.movedim(image, -1, -3)
+
+                if image.dtype != default_float_dtype:
+                    image = image.to(dtype=default_float_dtype).div(255)
+
+                image = ColorJitter(
+                    brightness=self.brightness,
+                    contrast=self.contrast,
+                    saturation=self.saturation,
+                    hue=self.hue,
+                )(image)
+
+                images[name] = image
+
+        return tensordict
---
+++ transforms/pointcloud_jitter_points.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+from typing import Sequence, Union
+
+import torch
+from tensordict import NonTensorData
+from torch_geometric.data import Data
+from torch_geometric.transforms import RandomJitter
+
+from environments.specs import DataSpecs
+from transforms.base_transform import KeyMapping, Transform
+
+
+class VariableJitter(RandomJitter):
+    def __init__(self, translate):
+        self.max_translate = translate
+        super().__init__(translate)
+
+    def __call__(self, data):
+        self.translate = torch.rand(1).item() * self.max_translate
+        return super().__call__(data)
+
+
+class JitterPointCloud(Transform):
+    def __init__(
+        self,
+        specs: DataSpecs,
+        translate: Union[float, int, Sequence[Union[float, int]]],
+        pcd_keys: str | Sequence[str] = "pcd",
+    ) -> None:
+
+        self.translate = translate
+
+        self.jitter = VariableJitter(translate)
+
+        self._specs = specs
+        if isinstance(pcd_keys, str):
+            pcd_keys = [pcd_keys]
+        else:
+            pcd_keys = list(pcd_keys)
+        self._pcd_keys = pcd_keys
+
+    @property
+    def key_mappings(self) -> list[KeyMapping]:
+        return [
+            KeyMapping(in_keys=[("obs", key)], out_keys=[("obs", key)])
+            for key in self._pcd_keys
+        ]
+
+    @property
+    def specs(self) -> DataSpecs:
+        return self._specs
+
+    def _call_one(self, nt_data: NonTensorData) -> Data:
+        data: Data = nt_data.data
+
+        pos = data.pos
+        assert pos is not None
+
+        data = self.jitter(data)
+
+        return data
