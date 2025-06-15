db0a719064aa4eaa193eaa8e6e29c47b981aa111
Emiliyan Gospodinov <gospodinov.emilian@gmail.com>
2025-06-15 16:21:13 +0200

    adapt physic's parameter to improve environment stability

--- environments/simulation/furniture_bench/insert_one_leg/furniture_bench_table_env_cfg.py
+++ environments/simulation/furniture_bench/insert_one_leg/furniture_bench_table_env_cfg.py
@@ -12,15 +12,12 @@ from isaaclab.assets import ArticulationCfg, AssetBaseCfg
 from isaaclab.envs import ManagerBasedRLEnvCfg
 from isaaclab.managers import ObservationGroupCfg as ObsGroup
 from isaaclab.managers import ObservationTermCfg as ObsTerm
-from isaaclab.managers import SceneEntityCfg
 from isaaclab.managers import TerminationTermCfg as DoneTerm
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.sensors.frame_transformer.frame_transformer_cfg import FrameTransformerCfg
 from isaaclab.sim.spawners.from_files.from_files_cfg import GroundPlaneCfg, UsdFileCfg
 from isaaclab.utils import configclass
-from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 from . import mdp
-from .mdp.observations import joint_pos, joint_pos_rel, joint_vel, joint_vel_rel
 
 BASE_PATH = os.path.dirname(__file__)
 
@@ -136,7 +133,7 @@ class InsertOneLegEnvCfg(ManagerBasedRLEnvCfg):
     """Configuration for the stacking environment."""
 
     # Scene settings
-    scene: FurnitureBenchTableSceneCfg = FurnitureBenchTableSceneCfg(num_envs=4096, env_spacing=2.5, replicate_physics=False)
+    scene: FurnitureBenchTableSceneCfg = FurnitureBenchTableSceneCfg(num_envs=1, env_spacing=2.5, replicate_physics=False)
     # Basic settings
     observations: ObservationsCfg = ObservationsCfg()
     actions: ActionsCfg = ActionsCfg()
@@ -152,14 +149,17 @@ class InsertOneLegEnvCfg(ManagerBasedRLEnvCfg):
     def __post_init__(self):
         """Post initialization."""
         # general settings
-        self.decimation = 2
+        # correspond to the number of simulation sub-steps between envionment steps
+        self.decimation = 3
         self.seed = 42 # set seed here for deterministic environments
         # self.sim.physx.use_gpu = False
         # Make it high to prevent environment reset when collection demonstrations
         # For training, set a lower value
         self.episode_length_s = 20.0
         # simulation settings
-        self.sim.dt = 0.01  # 100Hz
+        # WARNING: sim.dt != env.step_size/rendering step_size
+        # environment step_size = sim.dt / decimation !!!
+        self.sim.dt = 0.01  # 100Hz 
         self.sim.render_interval = self.decimation
         # GUI viewer perspective location
         # self.viewer.eye = (0.82762, 0.24943, 0.28195) # suitable for analyzing the area near the table assembly slots
@@ -168,22 +168,27 @@ class InsertOneLegEnvCfg(ManagerBasedRLEnvCfg):
         # self.viewer.lookat = (0.0, 0.0, -0.2) # suitable for analyzing the area near the table assembly slots
         self.viewer.lookat = (0.0, 0.0, 0.15) # suitable when recording videos for validation
 
-        self.sim.physx.solver_type = 1  # TGS solver (seems more stable)
-        self.sim.physx.bounce_threshold_velocity = 0.2
-        self.sim.physx.bounce_threshold_velocity = 0.01
+        # inspired from https://github.com/isaac-sim/IsaacLab/blob/3c3103f637f8193a363c4774781089baa19d0465/source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env_cfg.py#L101
+        # General solver recommendations: https://docs.omniverse.nvidia.com/extensions/latest/ext_physics/simulation-control/physics-settings.html#physics-solver
+        self.sim.physx.solver_type = 1  # TGS solver (seems more stable and faster converging than PGS)
+        self.sim.physx.bounce_threshold_velocity = 0.2 # m/s
         self.sim.physx.gpu_found_lost_aggregate_pairs_capacity = 1024 * 1024 * 4
         self.sim.physx.gpu_total_aggregate_pairs_capacity = 16 * 1024
         self.sim.physx.friction_correlation_distance = 0.00625
-
-        # taken from https://github.com/isaac-sim/IsaacLab/blob/3c3103f637f8193a363c4774781089baa19d0465/source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env_cfg.py#L101
         self.sim.max_position_iteration_count = 192 # Important to avoid interpenetration.
         self.sim.max_velocity_iteration_count = 1
         self.sim.gpu_max_rigid_contact_count = 2**30
         self.sim.gpu_max_rigid_patch_count = 2**30
         self.sim.gpu_max_num_partitions = 1 # Important for stable simulation.
-        # Set the friction seems to improve the overall simulation stability, but not everywhere, because objects become too sticky.
-            # TODO: Find bettter values for furniture-bench tasks.
-        # self.sim.physics_material = RigidBodyMaterialCfg(
-        #     static_friction=0.5, 
-        #     dynamic_friction=0.5,
-        # )
+
+        # The following fricition values seems to be the best trade-off between reaslism and simulation stability gains.
+        # Increasing both friction types leads to more stable simulation (in some edge-cases, e.g. when twisting the leg in the assembly slot further than necessary), but the objects starts become too sticky, which increases the sim-2-real gap! 
+        # The following MaterialCfg configures the defaults physic's material that PhyX is going to use per-default for all objects that don't have pre-defined physic's material
+        # In this case, this affects all environment assets. 
+        # If different physic's properties are desired, consider using the pre-defined event function "bind_physics_materials" as an event with mode "pre-startup".
+        self.sim.physics_material = RigidBodyMaterialCfg(
+            static_friction=0.5, 
+            dynamic_friction=0.5,
+            compliant_contact_damping=0.5,
+            compliant_contact_stiffness=0.5,
+        )
