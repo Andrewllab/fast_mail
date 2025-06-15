f4d31800ed456f20c958319d78171e1d81be0787
Emiliyan Gospodinov <gospodinov.emilian@gmail.com>
2025-06-15 16:16:24 +0200

    activate collision to the frankas, increase the solver_pos_iterations for more stability

--- environments/simulation/furniture_bench/insert_one_leg/assets/franka.py
+++ environments/simulation/furniture_bench/insert_one_leg/assets/franka.py
@@ -28,9 +28,9 @@ FRANKA_PANDA_CFG = ArticulationCfg(
             max_depenetration_velocity=5.0,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True, solver_position_iteration_count=16, solver_velocity_iteration_count=1
+            enabled_self_collisions=True, solver_position_iteration_count=100, solver_velocity_iteration_count=1
         ),
-        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
+        collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
         joint_pos={
@@ -98,9 +98,9 @@ FRANKA_MIMIC_PANDA_IMU_GRIPPERS_CFG = ArticulationCfg(
             max_depenetration_velocity=5.0,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True, solver_position_iteration_count=16, solver_velocity_iteration_count=1
+            enabled_self_collisions=True, solver_position_iteration_count=100, solver_velocity_iteration_count=1
         ),
-        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
+        collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
         joint_pos={
