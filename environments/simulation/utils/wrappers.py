53e5b0f320806f06f3b190da20ba5f5b6ebaa62c
Emiliyan Gospodinov <gospodinov.emilian@gmail.com>
2025-08-18 15:45:12 +0200

    refactor wrapper, in particular comments and action-preprocessing depending on the low-level controller and its mode

--- environments/simulation/utils/wrappers.py
+++ environments/simulation/utils/wrappers.py
@@ -22,9 +22,8 @@ class IsaacLabPreProcess(gym.Wrapper):
 
         self._obs_seq_len = obs_seq_len
 
-        # IK-Rel-controll uses 6d-delta pose + 1 gripper dim, but the model output 8d (3d-pos, 4d-quat, 1d-gripper)
         action_spec = ActionSpec(
-            action_dim=self.env.unwrapped.single_action_space.shape[-1] + 1, time=1
+            action_dim=self.env.unwrapped.single_action_space.shape[-1], time=1
         )
 
         # prepare the observation structure to match the input structure all models expects.
@@ -66,7 +65,7 @@ class IsaacLabPreProcess(gym.Wrapper):
                                     ].shape[
                                         -1
                                     ],
-                                    channel_order="CHW",  # per default IsaacLab returns HWC, but all RGB channels are automatically permuted to CHW. Check the self.preprocess_obs method.
+                                    channel_order="CHW",  # Per default, IsaacLab returns RGB-images with dimensions [B, H, W, C]: https://isaac-sim.github.io/IsaacLab/main/source/overview/core-concepts/sensors/camera.html#rgb-and-rgba. However, the channel order is automatically permuted in the self.preprocess_obs at each environment step.
                                     height=self.env.unwrapped.single_observation_space[
                                         obs_group_name
                                     ]["rgb"].shape[0],
@@ -79,8 +78,8 @@ class IsaacLabPreProcess(gym.Wrapper):
                                 camera_cfg["streams"]["depth"] = None
                                 camera_cfg["streams"]["depth"] = DepthStream(
                                     orthogonal=True,
-                                    channels=None,  # should be set to None, check DepthStream since it is assumed that depth image do not have a channel dim
-                                    channel_order="HW",
+                                    channels=None,  # should be set to None since a DepthStream assumes that a depth image does not have a channel dim
+                                    channel_order="HW",  # Per default, IsaacLab returns RGB-images with dimensions [B, H, W, C]: https://isaac-sim.github.io/IsaacLab/main/source/overview/core-concepts/sensors/camera.html#depth-and-distances. However, the channel order is automatically permuted in the self.preprocess_obs at each environment step.
                                     height=self.env.unwrapped.single_observation_space[
                                         obs_group_name
                                     ]["depth"].shape[0],
@@ -118,12 +117,12 @@ class IsaacLabPreProcess(gym.Wrapper):
                                 obs_group_name
                             ]["rgb"].shape[
                                 0
-                            ],  # !!!ASSUMPTION!!! All camera modalities have the same resolution
+                            ],  # !!!ASSUMPTION!!! All camera streams (rgb and depth) have the same resolution
                             width=self.env.unwrapped.single_observation_space[
                                 obs_group_name
                             ]["rgb"].shape[
                                 1
-                            ],  # !!!ASSUMPTION!!! All camera modalities have the same resolution
+                            ],  # !!!ASSUMPTION!!! All camera streams (rgb and depth) have the same resolution
                         ),
                         extrinsics=camera_cfg["extrinsics"],
                     )
@@ -147,7 +146,7 @@ class IsaacLabPreProcess(gym.Wrapper):
         return obs, info
 
     def step(self, action):
-        action = self.pre_process_action_orientation(action)
+        action = self._preprocess_action_orientation(action)
         obs, reward, terminated, truncated, info = self.env.step(action)
         obs = self._preprocess_obs(obs)
         info = self._preprocess_info(
@@ -174,13 +173,15 @@ class IsaacLabPreProcess(gym.Wrapper):
                     for obs_element_name, obs_element in obs_group.items():
                         match obs_element_name:
                             case "rgb":
-                                # [B, H, W, C] -> [B, C, H, W]
+                                # Per default, IsaacLab returns RGB-images with dimensions [B, H, W, C]: https://isaac-sim.github.io/IsaacLab/main/source/overview/core-concepts/sensors/camera.html#rgb-and-rgba
+                                # Permute [B, H, W, C] -> [B, C, H, W].
                                 pre_processed_obs[obs_group_name][obs_element_name] = (
                                     obs_element.permute(0, 3, 1, 2)
                                 )
                             case "depth":
+                                # Per default, IsaacLab returns Depth-images with dimensions [B, H, W, C] https://isaac-sim.github.io/IsaacLab/main/source/overview/core-concepts/sensors/camera.html#depth-and-distances
                                 # TODO: Remove: Balazs need to undo the permute anyway for depth images
-                                # [B, H, W, C] -> [B, C, H, W]
+                                # Permute [B, H, W, C] -> [B, C, H, W]
                                 pre_processed_obs[obs_group_name][obs_element_name] = (
                                     obs_element.permute(0, 3, 1, 2)
                                 )
@@ -192,11 +193,15 @@ class IsaacLabPreProcess(gym.Wrapper):
                                     0
                                 )  # Current convetion: Depth images does not have a channel dim. Also, gym_env_dataset.step_return_to_tensor_dict() adds additional batch dims
 
-        return TensorDict(pre_processed_obs, device=self.env.unwrapped.device)
+        return TensorDict(
+            pre_processed_obs,
+            device=self.env.unwrapped.device,
+        )
 
     def _preprocess_info(self, info, device, batch_dim):
-        """IsaacLab returns different dictionaries on reset and step
-        Make all elements vectorized tensors."""
+        """IsaacLab returns dictionaries with different dimensions on reset and step.
+        Transform all elements vectorized tensors.
+        """
 
         pre_processed_info = info["log"].copy()
         for k, v in pre_processed_info.items():
@@ -206,28 +211,60 @@ class IsaacLabPreProcess(gym.Wrapper):
                 v = v.view(batch_dim, -1)
             pre_processed_info[k] = v
 
-        return pre_processed_info
+        return TensorDict(pre_processed_info)
 
-    def pre_process_action_orientation(self, action: torch.Tensor):
-        """Preprocess action orientation representation
-        All models outputs action orientation as a quaternion, but IsaacLab environments expects euler angles
+    def _detect_ctrl_mode(self) -> str:
+        """Extract the controller and its mode from the environment nam."""
+        env_name = str(self.env.unwrapped.spec.id).lower()
+        if "ik-rel" in env_name:
+            return "ik_relative"
+        if "ik-abs" in env_name:
+            return "ik_absolute"
+        raise ValueError(
+            f"Not supported controll type. Currently, only IK-absolute and IK-relaltive controllers are supported in simulation."
+        )
+
+    def _preprocess_action_orientation(self, action: torch.Tensor):
+        """Preprocesses the policy's raw output to match the simulation control's mode. This way policy's output gets decoupled from the simulation control mode.
+        Args:
+            action : torch.Tensor
+                Raw policy output (B, 8): [x, y, z, qx, qy, qz, qw, grip]
+                Position units: meters
+                Orientation: quaternion (qx, qy, qz, qw)
+                grip: scalar in [0/1] (binary or continuous as configured)
+        Output:
+            IK-relative (B, 7): [dx, dy, dz, droll, dpitch, dyaw, grip]
+            IK-absolute (B, 8): [x, y, z, qx, qy, qz, qw, grip]
         """
+        control_mode = self._detect_ctrl_mode()
 
-        # import it here, otherwise not possible to import before Isaac-Sim starts
-        from isaaclab.utils.math import euler_xyz_from_quat, wrap_to_pi
+        match control_mode:
 
-        new_action = torch.zeros_like(action[:, :-1])
-        # copy 3d-position
-        new_action[:, :3] = action[:, :3]
-        # copy binary gripper action
-        new_action[:, -1] = action[:, -1]
-        # extract orientation as euler angles
-        d_roll, d_pitch, d_yall = euler_xyz_from_quat(action[:, 3:7])
-        d_roll = wrap_to_pi(d_roll)
-        d_pitch = wrap_to_pi(d_pitch)
-        d_yall = wrap_to_pi(d_yall)
-        new_action[:, 3] = d_roll
-        new_action[:, 4] = d_pitch
-        new_action[:, 5] = d_yall
+            case "ik_relative":
+                # import it here, otherwise not possible to import before Isaac-Sim starts
+                from isaaclab.utils.math import euler_xyz_from_quat, wrap_to_pi
 
-        return new_action
+                pre_processed_action = torch.zeros_like(action[:, :-1])
+                # copy 3d-position
+                pre_processed_action[:, :3] = action[:, :3]
+                # copy binary gripper action
+                pre_processed_action[:, -1] = action[:, -1]
+                # extract orientation as euler angles
+                d_roll, d_pitch, d_yall = euler_xyz_from_quat(action[:, 3:7])
+                # the computed euler angles are larger than pi for some rotations
+                d_roll = wrap_to_pi(d_roll)
+                d_pitch = wrap_to_pi(d_pitch)
+                d_yall = wrap_to_pi(d_yall)
+                pre_processed_action[:, 3] = d_roll
+                pre_processed_action[:, 4] = d_pitch
+                pre_processed_action[:, 5] = d_yall
+
+            case "ik_absolute":
+                pre_processed_action = action
+
+            case _:
+                raise ValueError(
+                    f"Not supported controll type. Currently, only IK-absolute and IK-relaltive controllers are supported in simulation."
+                )
+
+        return pre_processed_action
