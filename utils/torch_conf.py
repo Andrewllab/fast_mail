73254d9ec0a1f46b49f2085676cb2875168ceb85
Balazs Gyenes <balazsgyenes@users.noreply.github.com>
2025-08-19 19:11:35 +0200

    Set torch print options via config.

--- utils/torch_conf.py
+++ utils/torch_conf.py
@@ -11,3 +11,7 @@ def configure_torch(torch_cfg: DictConfig | None) -> None:
     if (precision := torch_cfg.get("set_float32_matmul_precision")) is not None:
         log.debug(f"Setting precision of float32 matmul to {precision}")
         torch.set_float32_matmul_precision(precision)
+
+    if print_opts := (torch_cfg.get("print_options", {}) or {}):
+        log.debug(f"Setting print options: {print_opts}")
+        torch.set_printoptions(**print_opts)
