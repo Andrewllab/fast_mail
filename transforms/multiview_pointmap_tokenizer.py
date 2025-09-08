Replace incorrect action normalization.
Fixes to pointmap cropping.
Fixes to platform configs.
Add compatibility for late fusion with spatial encoder.

transforms/multiview_pointmap_tokenizer.py
@@ -112,9 +112,9 @@ class MultiviewPointMapTokenizer(Transform, nn.Module):
 
         self.spatial_encoder = spatial_encoder
         if self.spatial_encoder is not None:
-            if fusion_type in ["6ch", "add", "cat"]:
-                raise ValueError(
-                    "Currently, fourier-features are only supported without a fusion."
+            if fusion_type == "6ch":
+                raise NotImplementedError(
+                    "A spatial encoder is not supported for 6ch fusion."
                 )
             in_channels = self.spatial_encoder.out_features
 
@@ -184,8 +184,11 @@ class MultiviewPointMapTokenizer(Transform, nn.Module):
                 image = tensordict["obs", key, name]
 
                 # apply fourier-feature encoder before dimension processing
-                if self.spatial_encoder is not None:
-                    # features: (B*N, D)
+                if self.spatial_encoder is not None and isinstance(
+                    stream, PointMapStream
+                ):
+                    # image: (B, T, H, W, C) -> (B, T, H, W, D)
+                    assert image.shape[-1] == self.spatial_encoder.in_features == 3
                     image = self.spatial_encoder(image)
 
                 if stream.channel_order == "HWC":
@@ -244,7 +247,7 @@ class MultiviewPointMapTokenizer(Transform, nn.Module):
         if self.late_fusion:
             # Do the same for rgb images
             rgb_images = torch.stack(rgb_streams, dim=-4)
-            assert shape == rgb_images.shape[-3:]
+            leading_dims, shape = (rgb_images.shape[:3], rgb_images.shape[-3:])
 
             if self.shared_encoder:
                 rgb_images = rgb_images.view(-1, *shape)

transforms/pointmap_crop.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from tensordict import TensorDict
 from torch import Tensor
 
-from environments.specs import DataSpecs, PointMapStream
+from environments.specs import CameraSpec, DataSpecs, PointMapStream
 from transforms.base_transform import Transform
 
 
@@ -31,6 +31,8 @@ class CropPointMap(Transform, nn.Module):
         y_range: tuple[float, float] | None = None,
         z_range: tuple[float, float] | None = None,
     ):
+        super().__init__()
+
         x_range = x_range or (-torch.inf, torch.inf)
         y_range = y_range or (-torch.inf, torch.inf)
         z_range = z_range or (-torch.inf, torch.inf)
@@ -50,7 +52,9 @@ class CropPointMap(Transform, nn.Module):
         return self._specs
 
     def forward(self, tensordict: TensorDict) -> TensorDict:
-        for key, spec in self._specs.items():
+        for key, spec in self._specs.obs.items():
+            if not isinstance(spec, CameraSpec):
+                continue
             for name, stream in spec.streams.items():
                 if not isinstance(stream, PointMapStream):
                     continue