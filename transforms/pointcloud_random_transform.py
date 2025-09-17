2c8fea234ad4493daa56e4f5a29ec4fdad6f5cd1
Balazs Gyenes <balazsgyenes@users.noreply.github.com>
2025-09-17 23:51:40 +0200

    Refactor randomly transform transforms.

--- transforms/pointcloud_random_transform.py
+++ transforms/pointcloud_random_transform.py
@@ -2,22 +2,15 @@ from __future__ import annotations
 
 from typing import Sequence
 
-import torch
 from tensordict import TensorDict
 from torch_geometric.data import Data
 
 from environments.specs import DataSpecs
-from transforms.base_transform import Transform
-from utils.math import (
-    make_pose,
-    quaternion_to_matrix,
-    random_orientation,
-    sample_uniform,
-    transform_pointcloud,
-)
+from transforms.pointmap_random_transform import BaseRandomlyTransform
+from utils.math import transform_pointcloud
 
 
-class RandomlyTransformPointCloudTrajectory(Transform):
+class RandomlyTransformPointCloudTrajectory(BaseRandomlyTransform):
     """Apply a random translation and rotation to the entire pointcloud
     trajectory during preprocessing, which is then constant throughout
     training.
@@ -30,11 +23,10 @@ class RandomlyTransformPointCloudTrajectory(Transform):
         self,
         specs: DataSpecs,
         max_translation: Sequence[float],
+        max_angle: float | Sequence[float] | None = None,
         pcd_keys: str | Sequence[str] = "pcd",
     ):
-        self.max_translation = torch.tensor(max_translation)
-        if self.max_translation.shape != (3,):
-            raise ValueError("max_translation must be a sequence of 3 floats")
+        super().__init__(max_translation, max_angle)
 
         self._specs = specs
         if isinstance(pcd_keys, str):
@@ -48,19 +40,9 @@ class RandomlyTransformPointCloudTrajectory(Transform):
         return self._specs
 
     def call_trajectory(self, tensordict: TensorDict) -> TensorDict:
-
         device = tensordict.device or "cpu"
 
-        quat = random_orientation(num=1, device=device)
-        translation = sample_uniform(
-            lower=-self.max_translation,
-            upper=self.max_translation,
-            size=(1, 3),
-            device="cpu",
-        ).to(device)
-
-        rot = quaternion_to_matrix(quat)
-        transform = make_pose(translation, rot).squeeze(0)
+        transform = self.random_pose(device)
 
         for key in self._pcd_keys:
             data: Data = tensordict["obs", key]
--- transforms/pointmap_random_transform.py
+++ transforms/pointmap_random_transform.py
@@ -9,16 +9,71 @@ from torch import Tensor
 from environments.specs import CameraSpec, DataSpecs, PointMapStream
 from transforms.base_transform import Transform
 from utils.math import (
+    euler_to_matrix,
     make_pose,
     quaternion_to_matrix,
     random_orientation,
     sample_uniform,
     transform_pointmap,
-    euler_to_matrix
 )
 
 
-class RandomlyTransformPointMapTrajectory(Transform):
+class BaseRandomlyTransform(Transform):
+    def __init__(
+        self,
+        max_translation: Sequence[float],
+        max_degrees: float | Sequence[float] | None = None,
+    ):
+        self.max_translation = torch.tensor(max_translation)
+        if self.max_translation.shape != (3,):
+            raise ValueError("max_translation must be a sequence of 3 floats")
+
+        if isinstance(max_degrees, (float, int)):
+            self.max_degrees = torch.tensor([max_degrees, max_degrees, max_degrees])
+        elif isinstance(max_degrees, Sequence):
+            self.max_degrees = torch.tensor(max_degrees)
+            if self.max_degrees.shape != (3,):
+                raise ValueError(
+                    "max_degrees must be a float or a sequence of 3 floats"
+                )
+        elif max_degrees is None:
+            self.max_degrees = None
+
+        if (
+            self.max_degrees is not None
+            and not ((0 <= self.max_degrees) & (self.max_degrees <= 180)).all().item()
+        ):
+            raise ValueError("max_degrees must be in [0, 180]")
+
+        # Convert to radians
+        if self.max_degrees is not None:
+            self.max_degrees = self.max_degrees * torch.pi / 180.0
+
+    def random_pose(self, device: str | torch.device) -> Tensor:
+        if self.max_degrees is None:
+            quat = random_orientation(num=1, device=device)
+            rot = quaternion_to_matrix(quat)
+        else:
+            random_euler = sample_uniform(
+                lower=-self.max_degrees,
+                upper=self.max_degrees,
+                size=(1, 3),
+                device=device,
+            )
+            rot = euler_to_matrix(random_euler, convention="XYZ")
+
+        translation = sample_uniform(
+            lower=-self.max_translation,
+            upper=self.max_translation,
+            size=(1, 3),
+            device="cpu",
+        ).to(device)
+
+        transform = make_pose(translation, rot).squeeze(0)
+        return transform
+
+
+class RandomlyTransformPointMapTrajectory(BaseRandomlyTransform):
     """Apply a random translation and rotation to the entire point map
     trajectory during preprocessing, which is then constant throughout
     training.
@@ -33,55 +88,18 @@ class RandomlyTransformPointMapTrajectory(Transform):
         max_translation: Sequence[float],
         max_angle: float | Sequence[float] | None = None,
     ):
-        self.max_translation = torch.tensor(max_translation)
-        if self.max_translation.shape != (3,):
-            raise ValueError("max_translation must be a sequence of 3 floats")
+        super().__init__(max_translation, max_angle)
 
         self._specs = specs
-        
-        if isinstance(max_angle, (float, int)):
-            self.max_angle = torch.tensor([max_angle, max_angle, max_angle])
-        elif isinstance(max_angle, Sequence):
-            self.max_angle = torch.tensor(max_angle)
-            if self.max_angle.shape != (3,):
-                raise ValueError("max_angle must be a float or a sequence of 3 floats")
-        elif max_angle is None:
-            self.max_angle = None
-
-        if self.max_angle is not None and not ((0 <= self.max_angle) & (self.max_angle <= 180)).all().item():
-            raise ValueError("max_angle must be in [0, 180]")
-        
-        # Convert to radians
-        if self.max_angle is not None:
-            self.max_angle = self.max_angle * torch.pi / 180.0
 
     @property
     def specs(self) -> DataSpecs:
         return self._specs
 
-    def __call__(self, tensordict: TensorDict) -> TensorDict:
+    def call_trajectory(self, tensordict: TensorDict) -> TensorDict:
         device = tensordict.device or "cpu"
 
-        if self.max_angle is None:
-            quat = random_orientation(num=1, device=device)
-            rot = quaternion_to_matrix(quat)
-        else:
-            random_euler = sample_uniform(
-                    lower=-self.max_angle,
-                    upper=self.max_angle,
-                    size=(1, 3),
-                    device=device,
-                )
-            rot = euler_to_matrix(random_euler, convention="XYZ")
-
-        translation = sample_uniform(
-            lower=-self.max_translation,
-            upper=self.max_translation,
-            size=(1, 3),
-            device="cpu",
-        ).to(device)
-
-        transform = make_pose(translation, rot)
+        transform = self.random_pose(device)
 
         for key, spec in self._specs.obs.items():
             if not isinstance(spec, CameraSpec):
@@ -94,4 +112,8 @@ class RandomlyTransformPointMapTrajectory(Transform):
                 pos = pointmap[..., :3]  # if it has colors, do not modify them
                 pointmap[..., :3] = transform_pointmap(pos, transform)
 
+        return tensordict
+
+    def __call__(self, tensordict: TensorDict) -> TensorDict:
+        # Do nothing if not called during preprocessing
         return tensordict
