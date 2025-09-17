9a2f2329c43dbe27b101aca48c484ba64e26c746
Balazs Gyenes <balazsgyenes@users.noreply.github.com>
2025-09-18 00:05:45 +0200

    Turn NormalizeDepth transform into NormalizingTransform.

--- transforms/depth_normalize.py
+++ transforms/depth_normalize.py
@@ -5,16 +5,16 @@ import logging
 import torch
 from tensordict import TensorDict
 
-from environments.specs import DataSpecs, CameraSpec, DepthStream
-from transforms.base_transform import Transform
+from environments.specs import CameraSpec, DataSpecs, DepthStream
+from transforms.base_transform import NormalizingTransform
 
 log = logging.getLogger(__name__)
 
 
-class DepthNormalize(Transform):
+class DepthNormalize(NormalizingTransform):
 
     def __init__(
-        self, 
+        self,
         specs: DataSpecs,
         max_depth: float,
     ):
@@ -23,29 +23,34 @@ class DepthNormalize(Transform):
         self._input_specs = {
             key: spec
             for key, spec in specs.obs.items()
-            if isinstance(spec, CameraSpec) and any(isinstance(s, DepthStream) for s in spec.streams.values())
+            if isinstance(spec, CameraSpec)
+            and any(isinstance(s, DepthStream) for s in spec.streams.values())
         }
 
         if not self._input_specs:
             raise ValueError("No depth streams found in the provided specs.")
-        
+
         self._specs = specs
 
         self.max_depth = max_depth
         if self.max_depth <= 0:
             raise ValueError("max_depth must be positive.")
-        
+
     @property
     def specs(self) -> DataSpecs:
         return self._specs
-            
+
+    def call_trajectory(self, tensordict: TensorDict) -> TensorDict:
+        # no need to collect statistics
+        return tensordict
+
     def __call__(self, tensordict: TensorDict) -> TensorDict:
-            
+
         for key, spec in self._input_specs.items():
             for name, stream in spec.streams.items():
                 if not isinstance(stream, DepthStream):
                     continue
-            
+
                 # Scale depth map from [0, max_depth] to [-1, 1]
                 depth_map: torch.Tensor = tensordict["obs", key][name]
                 depth_map = torch.clamp(depth_map, 0.0, self.max_depth)
@@ -53,4 +58,8 @@ class DepthNormalize(Transform):
                 depth_map = torch.clamp(depth_map, -1.0, 1.0)
                 tensordict["obs", key][name] = depth_map
 
+        return tensordict
+
+    def reverse(self, tensordict: TensorDict) -> TensorDict:
+        # no need to reverse a normalization on the observation
         return tensordict
