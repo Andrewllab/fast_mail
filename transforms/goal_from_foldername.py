Fix normalization bug and add random goal embeddings.
Add normalizing transform that assigns random embeddings to unique goal texts.
Run normalizing transform in forward mode during inference.
Fix action normalizers to no op when not training.
Add temporary fix to goal from folder name.
Update configs.
Disable FPS downsampling in DP3.

transforms/goal_from_foldername.py
@@ -28,6 +28,8 @@ class GoalTextFromFolderName(Transform):
     def __call__(self, tensordict: TensorDict) -> TensorDict:
 
         if "path" in tensordict:
+            # during training or eval on data, we extract the goal from the
+            # folder name
             path = Path(tensordict["path"])
             folder_name = path.parent.name
 
@@ -39,13 +41,19 @@ class GoalTextFromFolderName(Transform):
                 )
 
         elif self.current_task is not None:
+            # during rollout on an environment, we set the goal explicitly
             goal_text = self.goal_mapping[self.current_task]
+
+            # TODO: temporary fix
+            # During training, data items are collated, which turns string goal
+            # texts into a numpy array of strings. During rollout, collation
+            # is disabled, so we need to wrap the goal text into a numpy array here
+            goal_text = np.array([goal_text])
 
         else:
             raise RuntimeError(
                 "Tensordict does not contain `path` and current_task is unset."
             )
 
-        # tensordict["goal", self.key] = np.array([goal_text])
         tensordict["goal", self.key] = goal_text
         return tensordict