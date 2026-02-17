bbe1ec6ec7b3edade4985cde1d1a2c60cc0c6920
Balazs Gyenes <balazsgyenes@users.noreply.github.com>
2026-02-17 18:20:21 +0100

    Factor out point cloud renderer into util class.

--- transforms/render_pointcloud.py
+++ transforms/render_pointcloud.py
@@ -2,11 +2,7 @@ from __future__ import annotations
 
 import logging
 import multiprocessing as mp
-import traceback
 
-import numpy as np
-import open3d as o3d
-import open3d.visualization as o3dvis
 import torch
 from tensordict import NonTensorData, TensorDict
 from torch_geometric.data import Data
@@ -14,12 +10,13 @@ from torch_geometric.data import Data
 from environments.specs import CameraSpec, DataSpecs, PointCloudSpec
 from transforms.base_transform import Transform
 from utils.math import quaternion_to_matrix
+from utils.o3d import AsyncPcdRenderer
 
 ctx = mp.get_context("spawn")
 log = logging.getLogger(__name__)
 
 
-class RenderPointCloud(ctx.Process, Transform):
+class RenderPointCloud(Transform):
     def __init__(
         self,
         specs: DataSpecs,
@@ -28,7 +25,7 @@ class RenderPointCloud(ctx.Process, Transform):
         render_camera_poses: bool = False,
         render_ee_pose: bool = False,
         render_action: bool = False,
-        pose_frame_size: float = 0.1,
+        coordinate_frame_size: float = 0.1,
         log_pointcloud_size: bool = False,
         pcd_key: str = "pcd",
     ) -> None:
@@ -47,19 +44,49 @@ class RenderPointCloud(ctx.Process, Transform):
                 f"Key {pcd_key} is not a point cloud spec. Found {self._input_spec.type}"
             )
 
-        self.width = width
-        self.height = height
-        self.frame_size = pose_frame_size
-        self.log_pointcloud_size = log_pointcloud_size
+        self.renderer = AsyncPcdRenderer(
+            width=width,
+            height=height,
+            window_name=f"obs.{self._input_key}",
+            coordinate_frame_size=coordinate_frame_size,
+            log_pointcloud_size=log_pointcloud_size,
+        )
 
+        keys_poses_to_render = []
+        if render_ee_pose:
+            keys_poses_to_render.append(("obs", "ee_pose"))
+        if render_action:
+            keys_poses_to_render.append(("action",))
+        self.keys_poses_to_render = keys_poses_to_render
+
+        if render_camera_poses:
+            # Add coordinate frames for cameras with static extrinsics (no
+            # dynamic pose obs key) immediately since they won't change.
+            # Coordinate frames for moving cameras are rendered in each loop
+            # iteration, so skip them here.
+
+            for key, spec in self._specs.obs.items():
+                if (
+                    not isinstance(spec, CameraSpec)
+                    or spec.extrinsics is None
+                    or spec.dynamic_pose_obs_key is not None
+                ):
+                    # we only need to render cameras that have extrinsics
+                    # for moving cameras, we create a new coordinate frame in
+                    # each loop iteration, so skip it here
+                    continue
+
+                extrinsics = spec.extrinsics
+                rotation = extrinsics[:3, :3]
+                translation = extrinsics[:3, 3]
+
+                name = f"{key}_origin"
+                self.renderer.render_coordinate_frame(
+                    translation=translation, rotation=rotation, name=name
+                )
         self.render_camera_poses = render_camera_poses
-        self.render_ee_pose = render_ee_pose
-        self.render_action = render_action
 
         self._specs = specs
-
-        self.child_pipe, self.parent_pipe = ctx.Pipe(duplex=False)
-        self.start()
 
     @property
     def specs(self) -> DataSpecs:
@@ -69,34 +96,27 @@ class RenderPointCloud(ctx.Process, Transform):
         nt_data: NonTensorData = tensordict["obs"].get(self._input_key)
 
         data: Data = nt_data.data  # unpack NonTensorData wrapper around pyg Data object
-        # points, color = data_to_o3d(data)
 
-        points = data.pos
-        assert points is not None
-        points = points.cpu().numpy()  # rendering of cuda tensors is not supported
+        pos, color = data.pos, data.x
+        assert pos is not None
+        pos = pos.cpu().numpy()  # rendering of cuda tensors is not supported
 
-        if self.log_pointcloud_size:
-            log.debug(f"Rendered point cloud has {len(points)} points")
-
-        if (color := data.x) is not None:
+        if color is not None:
             color = color.cpu().numpy()
 
-        self.send_to_child(
-            (
-                "update_geometry",
-                {"type": "PointCloud", "name": "pcd", "points": points, "color": color},
-            )
-        )
+        self.renderer.render_pcd(pos, color, name=self._input_key)
 
         if self.render_camera_poses:
+            # Add coordinate frames for moving cameras (with dynamic pose obs key)
+
             for key, spec in self._specs.obs.items():
                 if (
                     not isinstance(spec, CameraSpec)
+                    or spec.extrinsics is None
                     or spec.dynamic_pose_obs_key is None
                 ):
                     continue
 
-                key = f"{key}_origin"
                 pose_key = spec.dynamic_pose_obs_key
                 if not isinstance(pose_key, tuple):
                     pose_key = (pose_key,)
@@ -117,210 +137,26 @@ class RenderPointCloud(ctx.Process, Transform):
                 rotation = extrinsics[:3, :3]
                 translation = extrinsics[:3, 3]
 
-                self.send_to_child(
-                    (
-                        "update_geometry",
-                        {
-                            "type": "CoordinateFrame",
-                            "name": key,
-                            "translation": translation,
-                            "rotation": rotation,
-                        },
-                    )
+                name = f"{key}_origin"
+                self.renderer.render_coordinate_frame(
+                    translation=translation, rotation=rotation, name=name
                 )
 
-        if self.render_ee_pose:
-            ee_pose = tensordict["obs", "ee_pose"].cpu()
-            # remove the batch dimension and index the last element
-            translation = ee_pose[0, -1, :3].numpy()
-            # `quaternion_to_matrix` requires a batch dimension, so leave it in
-            rotation = quaternion_to_matrix(ee_pose[0, -1:, 3:])
-            rotation = rotation.squeeze(dim=0).numpy()
+        for pose_key in self.keys_poses_to_render:
+            pose = tensordict.get(pose_key)
+            if pose is None:
+                # if the data doesn't exist for any reason, just skip it
+                continue
 
-            self.send_to_child(
-                (
-                    "update_geometry",
-                    {
-                        "type": "CoordinateFrame",
-                        "name": "ee_pose",
-                        "translation": translation,
-                        "rotation": rotation,
-                    },
-                )
-            )
+            # remove the batch dimension and index the last element in the sequence
+            pose = pose[0, -1].cpu()
 
-        if self.render_action:
-            action = tensordict["action"].cpu()
-            # remove the batch dimension and index the last element
-            translation = action[0, -1, :3].numpy()
-            # `quaternion_to_matrix` requires a batch dimension, so leave it in
-            rotation = quaternion_to_matrix(action[0, -1:, 3:7])
-            rotation = rotation.squeeze(dim=0).numpy()
+            translation = pose[:3].numpy()
+            rotation = quaternion_to_matrix(pose[3:7].unsqueeze(0)).squeeze(0).numpy()
 
-            self.send_to_child(
-                (
-                    "update_geometry",
-                    {
-                        "type": "CoordinateFrame",
-                        "name": "action",
-                        "translation": translation,
-                        "rotation": rotation,
-                    },
-                )
+            name = ".".join(pose_key)
+            self.renderer.render_coordinate_frame(
+                translation=translation, rotation=rotation, name=name
             )
 
         return tensordict
-
-    def send_to_child(self, msg: object) -> None:
-        try:
-            self.parent_pipe.send(msg)
-        except (BrokenPipeError, EOFError):
-            # rendering process has crashed or exited
-            raise KeyboardInterrupt("Open3D visualizer quit")
-
-    def run(self) -> None:
-
-        vis = o3dvis.Visualizer()
-        vis.create_window(f"obs.{self._input_key}", self.width, self.height)
-        geometries = {}
-
-        # add an extra large coordinate frame at the origin
-        origin = o3d.geometry.TriangleMesh.create_coordinate_frame(
-            size=self.frame_size * 3, origin=[0, 0, 0]
-        )
-        vis.add_geometry(origin)
-        geometries["origin"] = origin
-
-        if self.render_camera_poses:
-            # add coordinate frames for cameras
-            for key, spec in self.specs.obs.items():
-                if (
-                    not isinstance(spec, CameraSpec)
-                    or spec.extrinsics is None
-                    or spec.dynamic_pose_obs_key is not None
-                ):
-                    # we only need to render cameras that have extrinsics
-                    # for moving cameras, we create a new coordinate frame in
-                    # each loop iteration, so skip it here
-                    continue
-
-                key = f"{key}_origin"
-                extrinsics = spec.extrinsics
-                rotation = extrinsics[:3, :3]
-                translation = extrinsics[:3, 3]
-
-                camera_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
-                    size=self.frame_size,
-                    origin=translation,
-                )
-                camera_frame.rotate(rotation, center=translation)
-
-                vis.add_geometry(camera_frame)
-                geometries[key] = camera_frame
-
-        try:
-            while True:
-                # Non-blocking check for incoming messages
-                if self.child_pipe.poll():
-                    cmd, data = self.child_pipe.recv()
-                    if cmd == "QUIT":
-                        break
-
-                    elif cmd == "update_geometry":
-                        match data["type"]:
-                            case "PointCloud":
-                                name = data["name"]
-                                points = data["points"]
-                                points = o3d.utility.Vector3dVector(points)
-                                color = data["color"]
-                                if color is not None:
-                                    if color.dtype == np.uint8:
-                                        color = color.astype(np.float32) / 255.0
-                                    color = o3d.utility.Vector3dVector(color)
-
-                                if name in geometries:
-                                    pcd = geometries[name]
-                                    pcd.points = points
-                                    if color is not None:
-                                        pcd.colors = color
-                                    vis.update_geometry(pcd)
-
-                                else:
-                                    # on first call
-                                    pcd = o3d.geometry.PointCloud(points)
-                                    if color is not None:
-                                        pcd.colors = color
-                                    vis.add_geometry(pcd)
-                                    geometries[name] = pcd
-
-                            case "CoordinateFrame":
-                                name = data["name"]
-                                translation = data["translation"]
-                                rotation = data["rotation"]
-
-                                # since we can't set an absolute pose, remove the old coordinate
-                                # frame and add a new one with the correct pose
-                                try:
-                                    frame = geometries[name]
-                                    vis.remove_geometry(frame, reset_bounding_box=False)
-                                except KeyError:
-                                    pass
-
-                                frame = (
-                                    o3d.geometry.TriangleMesh.create_coordinate_frame(
-                                        size=self.frame_size,
-                                        origin=translation,
-                                    )
-                                )
-                                frame.rotate(rotation, center=translation)
-                                vis.add_geometry(frame, reset_bounding_box=False)
-                                geometries[name] = frame
-
-                            case _:
-                                print(f"Unknown geometry type: {data['type']}")
-
-                    else:
-                        print(f"Unknown command: {cmd}")
-
-                # Keep the window responsive
-                vis.poll_events()
-                vis.update_renderer()
-
-        except KeyboardInterrupt:
-            pass
-        except Exception:
-            traceback.print_exc()
-        finally:
-            try:
-                vis.destroy_window()
-            except Exception:
-                pass
-
-            self.child_pipe.close()
-
-    def close(self) -> None:
-        self.send_to_child(("QUIT", {}))
-        self.terminate()
-        self.join()
-        self.parent_pipe.close()
-
-
-def data_to_o3d(
-    data: Data,
-) -> tuple[o3d.utility.Vector3dVector, o3d.utility.Vector3dVector | None]:
-    """
-    Convert a Data object to an Open3D PointCloud.
-    """
-    points = data.pos.cpu()  # rendering of cuda tensors is not supported
-    assert points is not None
-    points = o3d.utility.Vector3dVector(points.numpy())
-
-    if data.x is not None:
-        color = data.x.cpu()
-        if color.dtype == torch.uint8:
-            color = color.float() / 255.0
-        color = o3d.utility.Vector3dVector(color.numpy())
-    else:
-        color = None
-
-    return points, color
--- utils/o3d.py
+++ utils/o3d.py
@@ -1,7 +1,15 @@
+import logging
+import multiprocessing as mp
+import traceback
+
 import numpy as np
 import open3d as o3d
 import open3d.core as o3c
+import open3d.visualization as o3dvis
 import torch.utils.dlpack
+
+ctx = mp.get_context("spawn")
+log = logging.getLogger(__name__)
 
 
 def torch_to_o3d(tensor: torch.Tensor) -> o3c.Tensor:
@@ -23,3 +31,173 @@ def o3d_to_torch(o3d_tensor: o3c.Tensor) -> torch.Tensor:
     Convert an Open3D tensor to a pytorch tensor.
     """
     return torch.utils.dlpack.from_dlpack(o3d_tensor.to_dlpack())
+
+
+class AsyncPcdRenderer(ctx.Process):
+    def __init__(
+        self,
+        width: int = 1024,
+        height: int = 768,
+        window_name: str = "PointCloud",
+        coordinate_frame_size: float = 0.1,
+        log_pointcloud_size: bool = False,
+        show_origin: bool = True,
+    ) -> None:
+
+        super().__init__(daemon=True)
+
+        self.width = width
+        self.height = height
+        self.window_name = window_name
+        self.coordinate_frame_size = coordinate_frame_size
+        self.log_pointcloud_size = log_pointcloud_size
+        self.show_origin = show_origin
+
+        self.child_pipe, self.parent_pipe = ctx.Pipe(duplex=False)
+        self.start()
+
+    def render_pcd(
+        self, points: np.ndarray, colors: np.ndarray | None, name: str = "pcd"
+    ) -> None:
+        if self.log_pointcloud_size:
+            log.info(f"Rendering point cloud '{name}' with {points.shape[0]} points.")
+
+        self._send_to_child(
+            (
+                "update_geometry",
+                {
+                    "type": "PointCloud",
+                    "name": name,
+                    "points": points,
+                    "color": colors,
+                },
+            )
+        )
+
+    def render_coordinate_frame(
+        self,
+        translation: np.ndarray,
+        rotation: np.ndarray,
+        name: str,
+        size: float | None = None,
+    ) -> None:
+        self._send_to_child(
+            (
+                "update_geometry",
+                {
+                    "type": "CoordinateFrame",
+                    "name": name,
+                    "translation": translation,
+                    "rotation": rotation,
+                    "size": size or self.coordinate_frame_size,
+                },
+            )
+        )
+
+    def _send_to_child(self, msg: object) -> None:
+        try:
+            self.parent_pipe.send(msg)
+        except (BrokenPipeError, EOFError):
+            # rendering process has crashed or exited
+            raise KeyboardInterrupt("Open3D visualizer quit")
+
+    def run(self) -> None:
+
+        vis = o3dvis.Visualizer()
+        vis.create_window(self.window_name, self.width, self.height)
+        geometries = {}
+
+        if self.show_origin:
+            # add an extra large coordinate frame at the origin
+            origin = o3d.geometry.TriangleMesh.create_coordinate_frame(
+                size=self.coordinate_frame_size * 3, origin=[0, 0, 0]
+            )
+            vis.add_geometry(origin)
+            geometries["origin"] = origin
+
+        try:
+            while True:
+                # Non-blocking check for incoming messages
+                if self.child_pipe.poll():
+                    cmd, data = self.child_pipe.recv()
+                    if cmd == "QUIT":
+                        break
+
+                    elif cmd == "update_geometry":
+                        match data["type"]:
+                            case "PointCloud":
+                                name = data["name"]
+                                points = data["points"]
+                                points = o3d.utility.Vector3dVector(points)
+                                color = data["color"]
+                                if color is not None:
+                                    if color.dtype == np.uint8:
+                                        color = color.astype(np.float32) / 255.0
+                                    color = o3d.utility.Vector3dVector(color)
+
+                                if name in geometries:
+                                    pcd = geometries[name]
+                                    pcd.points = points
+                                    if color is not None:
+                                        pcd.colors = color
+                                    vis.update_geometry(pcd)
+
+                                else:
+                                    # on first call
+                                    pcd = o3d.geometry.PointCloud(points)
+                                    if color is not None:
+                                        pcd.colors = color
+                                    vis.add_geometry(pcd)
+                                    geometries[name] = pcd
+
+                            case "CoordinateFrame":
+                                name = data["name"]
+                                translation = data["translation"]
+                                rotation = data["rotation"]
+                                size = data["size"]
+
+                                # since we can't set an absolute pose, remove the old coordinate
+                                # frame and add a new one with the correct pose
+                                try:
+                                    frame = geometries[name]
+                                    vis.remove_geometry(frame, reset_bounding_box=False)
+                                except KeyError:
+                                    pass
+
+                                frame = (
+                                    o3d.geometry.TriangleMesh.create_coordinate_frame(
+                                        size=size,
+                                        origin=translation,
+                                    )
+                                )
+                                frame.rotate(rotation, center=translation)
+                                vis.add_geometry(frame, reset_bounding_box=False)
+                                geometries[name] = frame
+
+                            case _:
+                                print(f"Unknown geometry type: {data['type']}")
+
+                    else:
+                        print(f"Unknown command: {cmd}")
+
+                # Keep the window responsive
+                vis.poll_events()
+                vis.update_renderer()
+
+        except KeyboardInterrupt:
+            pass
+        except Exception:
+            traceback.print_exc()
+        finally:
+            try:
+                vis.destroy_window()
+            except Exception:
+                pass
+
+            self.child_pipe.close()
+
+    def close(self) -> None:
+        self._send_to_child(("QUIT", {}))
+        self.terminate()
+        self.join()
+        self.parent_pipe.close()
